bacb5c3bca4bd492d2e41b1b6ceda1425ad71ec0
LuizFernandoForte <lfforte@gmail.com>
2023-12-05 11:29:22 -0300

    Solução Prática 2

--- script.js
+++ script.js
@@ -21,14 +21,23 @@ console.log(`A soma das notas do Bimestre ${i+1} é ${notasSomaBimestre[i]}\na m
 
 //###-------------------------------Prática 2
 
+const notasDoAno = [[9,8,6,8],[5,8,4,3],[8,2,6,7],[9,9,9,7]];
+const notasSomaBimestre =[];
+let bimestre = 0;
 
+for (let arr in notasDoAno){
+  let somaBimestre = 0;
+  const arrayNotas = [] = notasDoAno[bimestre];
 
+  for (let j = 0; j < arrayNotas.length; j++){
+    somaBimestre += arrayNotas[j];
+  }
 
+notasSomaBimestre.push(somaBimestre)
+console.log(`A soma das notas do Bimestre ${bimestre+1} é ${notasSomaBimestre[bimestre]}\na média do Bimestre ${bimestre+1} = ${notasSomaBimestre[bimestre]/4} \n`);
 
-
-
-
-
+bimestre++;
+};
 
 
 
